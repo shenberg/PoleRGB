bbb113bfbd2d5358e5d05c71444ce493971df42c
Roee Shenberg <shenberg@gmail.com>
2015-04-10 13:20:45 +0300

    Works on teensy 3.1, will stop displaying image on getting tcp packet to 10.0.0.69:1000

--- PoleRGB.ino
+++ PoleRGB.ino
@@ -1,55 +1,70 @@
-#include "SPI.h"
-#include "WS2801.h"
-
+#include "FastLED.h"
 #include "logo.h"
+#include <UIPEthernet.h>
+
+EthernetServer server = EthernetServer(1000);
+
+#define CLOCK_PIN 20
+#define DATA_PIN 21
 
 #define NUM_PIXELS 72
-// Set the first variable to the NUMBER of pixels. 25 = 25 pixels in a row
-WS2801 strip = WS2801(NUM_PIXELS);
+CRGB pixels[NUM_PIXELS] = {0};
+
+CLEDController *ledController;
+
 
 void setup() {
-  strip.begin();
-  strip.show();
-
   Serial.begin(9600);
+
+  ledController = &FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN>(pixels, NUM_PIXELS);
+
+  uint8_t mac[6] = {0x00,0x01,0x02,0x03,0x04,0x05};
+  IPAddress myIP(10,0,0,69);
+
+  Ethernet.begin(mac,myIP);
+
+  server.begin();
 }
 
+bool flag=false;
 void loop() {
- #if 0
-  // Some example procedures showing how to display to the pixels:
-  colorWipe(strip.Color(255, 0, 0), 50); // Red
-  colorWipe(strip.Color(0, 255, 0), 50); // Green
-  colorWipe(strip.Color(0, 0, 255), 50); // Blue
-  // Send a theater pixel chase in...
-  theaterChase(strip.Color(127, 127, 127), 50); // White
-  theaterChase(strip.Color(127,   0,   0), 50); // Red
-  theaterChase(strip.Color(  0,   0, 127), 50); // Blue
 
-  rainbow(20);
-  rainbowCycle(20);
-  theaterChaseRainbow(50);
- #endif
-// updateState();
-// stateToPixels(5);
   showImage();
 
-  for(int i = 0; i > NUM_PIXELS; i++) {
-    strip.setPixelColor(i, 0);
-  }
-  strip.show();
-  delay(40);
+  size_t size;
+
+  if (EthernetClient client = server.available())
+    {
+      while((size = client.available()) > 0)
+        {
+          uint8_t* msg = (uint8_t*)malloc(size);
+          size = client.read(msg,size);
+          Serial.write(msg,size);
+          free(msg);
+        }
+      client.println("DATA from Server!");
+      client.stop();
+      flag=true;
+    }
 }
 
 
 
 void showImage() {
+  unsigned long start = millis();
   for(int col = 0; col < IMAGE_COLUMNS; col++) {
      /*for(int row = 0; row < NUM_PIXELS; row++) {
        strip.setPixelColor(row, pgm_read_dword( &picture[col*IMAGE_ROWS + row]));
        //strip.setPixelColor(row, 0,255,0);
      }
      strip.show();*/
-     strip.showProgmem((uint16_t)&picture[col*IMAGE_ROWS*PIXEL_SIZE]);
-     //delay(3);
+     //if (!flag) memcpy(pixels, &picture[col*IMAGE_ROWS*PIXEL_SIZE], NUM_PIXELS*3);
+
+     CRGB *addr = flag ? (CRGB *)&picture[col*IMAGE_ROWS*PIXEL_SIZE] : pixels;
+     ledController->show(addr, NUM_PIXELS, 255);
   }
+  unsigned long end = millis();
+  ledController->show(pixels,NUM_PIXELS, 255);
+  delay(40);
+  Serial.print("columns = "); Serial.print(IMAGE_COLUMNS); Serial.print(", time= "); Serial.println((end-start));
 }
